3a45985fe811b7b4c82204ba2e4e2e3730c31279
Drew S <andrewsantos2022@u.northwestern.edu>
2022-11-30 22:07:06 -0600

    added loop through randomString array and added to password, finished if statements, tested working

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-let number = ['0','1','2','3','4','5','6','7','8','9']
-let lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-let upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-let specialChar = ['~','!','@','#','$','%','^','&','*','(',')','-','+','=','_']
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 //var for all lowercase
 //var for all uppercase
 //var for all specialchar
 //var for all numbers
+let number = ['0','1','2','3','4','5','6','7','8','9'];
+let lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let specialChar = ['~','!','@','#','$','%','^','&','*','(',')','-','+','=','_'];
+let password = [];
+
+
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+
 
 //var with empty string/array for container
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 function generatePassword() {
-  let randomString = ''
-
+  let randomString = [];
   //PROMPT password length
+  let passwordLength = prompt("Choose a number between 8 and 124 for your password length.");
+  if (passwordLength < 8 || passwordLength > 128) {
+  alert ("incorrect password length. choose a number between 8 and 124.")
+  }
+  let numChoice = window.confirm("Would you like to include numbers in your password?");
+  let lowerChoice = window.confirm("Would you like to include lower case letters?");
+  let upperChoice = window.confirm("Would you like to include upper case letters?");
+  let specialChoice = window.confirm("Would you like to include special characters?");
   //if - length is not between 8 and 128, or if it's not a number
   //alert() - incorrect length
   //prompt them again - hint: call the function again
@@ -35,19 +35,62 @@ function generatePassword() {
     //confirm uppercase
     //confirm specialchar
     //confirm numbers
+    
 
     //if(lowercase) - add lowercaseChars to empty container var 
     //if(uppercase) - add uppcaseChrs to empty container var 
     //if(specialchar) - add specialchars to empty container var 
     //if(numbers) - add numberchars to empty container var 
+    if (numChoice === true)  {
+      randomString = randomString.concat(number);
+      let maximum = number.length;
+      let rnd = Math.floor(Math.random() * maximum);
+      password.push(number[rnd]);
+      passwordLength--;
+    }
+    if (lowerChoice === true)  {
+      randomString = randomString.concat(lowerCase);
+      let maximum = lowerCase.length;
+      let rnd = Math.floor(Math.random() * maximum);
+      password.push(lowerCase[rnd]);
+      passwordLength--;
+    }
+    if (upperChoice === true)  {
+      randomString = randomString.concat(upperCase);
+      let maximum = upperCase.length;
+      let rnd = Math.floor(Math.random() * maximum);
+      password.push(upperCase[rnd]);
+      passwordLength--;
+    }
+    if (specialChoice === true)  {
+      randomString = randomString.concat(specialChar);
+      let maximum = specialChar.length;
+      let rnd = Math.floor(Math.random() * maximum);
+      password.push(specialChar[rnd]);
+      passwordLength--;
+    }
+  
 
 //for loop, loop through the container var with the lenth user chose
 //each time, select a random character and add to randomString
 
   //}
-
-
-  return randomString;
+for (let i = 0; i < passwordLength; i++) {
+  let maximum = randomString.length;
+  let rnd = Math.floor(Math.random() * maximum)
+  password.push(randomString[rnd]);
 }
+return password
+}
+
+// Write password to the #password input
+function writePassword() {
+  generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password.join("");
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
